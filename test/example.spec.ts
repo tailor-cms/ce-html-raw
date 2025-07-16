0df3182ace2ed7a826d8e3c0710f74ead52e0f50
underscope <underscope@gmail.com>
2025-07-16 14:43:50 +0200

    💄

--- test/example.spec.ts
+++ test/example.spec.ts
@@ -1,11 +1,6 @@
 import { expect, test } from '@playwright/test';
 import { pom } from '@tailor-cms/cek-e2e';
 
-const COPY = {
-  edit: 'Edit version of the content element',
-  display: 'Display version of the content element',
-};
-
 test.beforeEach(async ({ page }) => {
   await page.goto('/');
   await page.waitForLoadState('networkidle');
@@ -15,13 +10,11 @@ test('Renders Edit component', async ({ page }) => {
   const editPanel = new pom.EditPanel(page);
   await editPanel.persistFocus();
   await expect(editPanel.editor).toBeVisible();
-  await expect(editPanel.editor.getByText(COPY.edit)).toBeVisible();
 });
 
 test('Renders Display component', async ({ page }) => {
   const displayPanel = new pom.DisplayPanel(page);
   await expect(displayPanel.editor).toBeVisible();
-  await expect(displayPanel.editor.getByText(COPY.display)).toBeVisible();
 });
 
 test('Renders server state panel', async ({ page }) => {
