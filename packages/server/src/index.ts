b12f5d9d39c3eb2b513bc8dd657f8bd6858a3c39
rkusan00 <renato.kusanovic@gmail.com>
2026-08-12 15:03:38 +0200

    Migrate to the latest CEK version which introduced general feedback

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,15 +1,12 @@
-import { ai, initState, type } from '@tailor-cms/ce-html-raw-manifest';
 import type { HookMap, ServerModule } from '@tailor-cms/cek-common';
 import type { Element } from '@tailor-cms/ce-html-raw-manifest';
+import manifest from '@tailor-cms/ce-html-raw-manifest';
 
 export const hookMap: HookMap<Element> = new Map();
 
 const serverModule: ServerModule<Element> = {
-  type,
-  initState,
+  ...manifest,
   hookMap,
-  ai,
 };
 
 export default serverModule;
-export { type, initState, ai };
